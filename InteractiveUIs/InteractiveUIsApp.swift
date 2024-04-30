7da1a74f14fd406cdbec55bab62299a603a141f3
Anne Do <doanneda@gmail.com>
2024-04-30 13:20:55 -0700

    Initial Commit

---
+++ InteractiveUIs/InteractiveUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsApp.swift
+//  InteractiveUIs
+//
+//  Created by Scholar on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
